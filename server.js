ef0ef1b605efe0efcbe20569c8b4152cb7a3028a
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-07 16:27:56 -0600

    added more boiler plate and added routes folder with files

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use('/', routes);
 
 app.use(express.static('public'));
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
